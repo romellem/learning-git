bd75ee2bafe98757a8632472efda6c8ce141efcb
Matt Wade <matthew.wade@sproutsocial.com>
2021-07-07 10:23:29 -0500

    Adds initial file

---
+++ file.js
@@ -0,0 +1,22 @@
+// Fizzbuzz Solution
+function fizzBuzz() {
+	for (let i = 0; i < 100; i++) {
+		if (i % 15 == 0) {
+			console.log("FizzBuzz");
+		} else if (i % 3 == 0) {
+			console.log("Fizz");
+		} else if (i % 5 == 0) {
+			console.log("Buzz");
+		} else {
+			console.log(i);
+		}
+	}
+}
+
+function isTrue(value) {
+	if (value === true) {
+		return true;
+	} else {
+		return false;
+	}
+}
