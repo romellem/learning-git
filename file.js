7ef87a18e6ff12f12139fdb71a5718649408a9d9
bfoehring <bill.foehring@sproutsocial.com>
2021-07-07 10:40:00 -0500

    Updates fizzbuzz to take in optional argument

--- file.js
+++ file.js
@@ -1,6 +1,6 @@
 // Fizzbuzz Solution
-function fizzBuzz() {
-	for (let i = 0; i < 100; i++) {
+function fizzBuzz(num = 100) {
+	for (let i = 0; i < num; i++) {
 		if (i % 15 == 0) {
 			console.log("FizzBuzz");
 		} else if (i % 3 == 0) {
