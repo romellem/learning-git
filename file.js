38422bc304b6b3d4c406a71a70358b2bf91afb75
Matt Wade <matthew.wade@sproutsocial.com>
2021-07-28 10:50:08 -0500

    Uses strict equality check

--- file.js
+++ file.js
@@ -1,11 +1,17 @@
 // FizzBuzz Solution
+<<<<<<< HEAD
 function fizzBuzz(num = 100) {
 	for (let i = 0; i < num; i++) {
 		if (i % (3 * 5) == 0) {
+=======
+function fizzBuzz() {
+	for (let i = 0; i < 100; i++) {
+		if (i % (3 * 5) === 0) {
+>>>>>>> c8ae4c8 (Uses strict equality check)
 			console.log("FizzBuzz");
-		} else if (i % 3 == 0) {
+		} else if (i % 3 === 0) {
 			console.log("Fizz");
-		} else if (i % 5 == 0) {
+		} else if (i % 5 === 0) {
 			console.log("Buzz");
 		} else {
 			console.log(i);
